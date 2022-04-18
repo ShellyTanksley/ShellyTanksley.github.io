b03d3fa8b2f4e19072ac8a867875bd6a9fd13b53
ShellyTanksley <100870125+ShellyTanksley@users.noreply.github.com>
2022-04-18 09:51:00 -0500

    Added New Table

--- assets/script/ap.js
+++ assets/script/ap.js
@@ -7,13 +7,17 @@
 	var liveprice = {
 		"async":true,
 		"scroosDomain": true,
-		"url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+		/*"url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",*/
+		"url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true",
 		"method":"GET",
 		"headers":{}
 	}
 	$.ajax(liveprice).done(function(response){
 	///console.log.(response);
-	swbl.innerHTML = response.dogecoin.usd;	
+	/*swbl.innerHTML = response.dogecoin.usd_24h_change;*/
+	swbl.innerHTML = response.dogecoin.usd;		
 	btc.innerHTML = response.bitcoin.usd;
 	eth.innerHTML = response.ethereum.usd;	
 	});	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
